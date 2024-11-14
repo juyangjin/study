da5e9bcbfe781d81fa48c9d5e9bef149aebc9de2
joyangjin <wndid2008@gmail.com>
2024-11-14 10:09:45 +0900

    :tada:feat:Calculator step 1

---
+++ 2024/24_11_14/Calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static double calculate(String operator, int firstNumber, int secondNumber){
+        int result = 0;
+
+        if(operator.equals("+")){
+            result = firstNumber + secondNumber;
+        }else if(operator.equals("-")){
+            result = firstNumber - secondNumber;
+        }else if(operator.equals("*")){
+            result = firstNumber * secondNumber;
+        }else if(operator.equals("/")){
+            result = firstNumber / secondNumber;
+        }
+        System.out.println("결과는 " + result + "입니다.");
+        return 0;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("정수 두 개를 입력하시오");
+        int firstNumber = sc.nextInt();
+        int secondNumber = sc.nextInt();
+        System.out.println("+,-,*,/ 중에 한 가지 연산자를 입력하시오");
+        String tmp = sc.nextLine(); // 스캐너 문제로 한 줄 tmp 입력
+        String operator = sc.nextLine();
+
+        calculate(operator,firstNumber,secondNumber);
+
+    }
+}
