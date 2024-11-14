5511807a2107c7d7ae9bfed5fb3a35bd8f3f2fb0
joyangjin <wndid2008@gmail.com>
2024-11-14 16:22:39 +0900

    :zap:perf:add exception

---
+++ 2024/24_11_14/Calculator_exception_ver/src/main/java/org/example/AddOperation.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class AddOperation extends AbstractOperation{
+    @Override
+    public double operate(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+
+}
