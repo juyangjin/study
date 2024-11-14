2ad8fa7c4e91a9fafbe72f056e6ddf5bc19d53ba
Juyang_Jin <112486031+juyangjin@users.noreply.github.com>
2024-11-14 17:02:07 +0900

    :art:zap:코드수정

--- 2024/24_11_14/Calculator/src/main/java/org/example/AbstractOperation.java
+++ 2024/24_11_14/Calculator/src/main/java/org/example/AbstractOperation.java
@@ -1,7 +1,5 @@
 package org.example;
 
 public abstract class AbstractOperation{
-    public double operate(int firstNumber, int secondNumber) {
-        return 0;
-    }
+    public double operate(int firstNumber, int secondNumber);
 }
