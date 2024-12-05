056e2e5bff1fdfd1b20dda1c893ed7a14a0c995a
joyangjin <wndid2008@gmail.com>
2024-12-05 17:51:12 +0900

    :sparkles:feat:add feat Arrayfind, titleupdate, allupdate

--- 2024/24_12_05/memo/src/main/java/org/example/memo/dto/MemoRequestDto.java
+++ 2024/24_12_05/memo/src/main/java/org/example/memo/dto/MemoRequestDto.java
@@ -3,7 +3,7 @@ package org.example.memo.dto;
 import lombok.Getter;
 
 @Getter
-public class MemoRequestDto{
+public class MemoRequestDto {
     private String title;
     private String contents;
 }
